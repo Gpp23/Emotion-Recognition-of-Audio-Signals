1b2759660a10fd5bad3b3d9e93fb10285bd6a3a8
Giuseppe Condorelli <giuseppecondorelli23@gmail.com>
2022-10-26 18:27:34 +0200

    Added files

---
+++ Emotion Recognition of Audio Signals/include/audioEmotionClassificator.py
@@ -0,0 +1,229 @@
+from asyncio.windows_events import NULL
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+import warnings
+
+warnings.filterwarnings("ignore")
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder, StandardScaler, RobustScaler
+import pandas as pd
+import numpy as np
+import seaborn as sn
+
+from include import audioFeatures as af
+from IPython.display import Audio
+from IPython.display import display
+
+import librosa
+
+import librosa.display
+
+from datetime import timedelta
+
+import pyaudio
+import wave
+
+
+class ERAS(object):
+    def __init__(self, *args):
+        super(ERAS, self).__init__(*args)
+        ERAS.__model = None
+        ERAS.__scaler = None
+        ERAS.__encoder = None
+
+
+    def __dataPreProcessing(dataset, categories, scaler = StandardScaler(), test_size = 0.2):
+        
+        data = pd.read_csv(dataset, engine='python')
+        data.head()
+        
+        emotion_list = data.iloc[:, -1]
+        encoder = LabelEncoder()
+        encoder = encoder.fit(categories)
+        y = encoder.transform(emotion_list)
+        
+        X = np.array(data.iloc[:, :-1], dtype = float)
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = y, test_size = test_size)
+        
+        scaler = scaler.fit(X_train)
+        
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+        
+        return X_train, X_test, y_train, y_test, scaler, encoder
+
+    def __modelScore(categories, model, X_test, y_test):
+            
+        prediction = model.predict(X_test)
+
+        print(classification_report(prediction, y_test, target_names = categories))
+        df_cm = pd.DataFrame(confusion_matrix(prediction, y_test, normalize='true'), index = [i for i in categories],
+                            columns = [i for i in categories])
+        plt.figure(figsize = (10,7))
+        sn.heatmap(df_cm, annot=True)
+
+        plt.show()
+
+    def trainModel(self, dataset, categories = "Angry Calm Dark Dramatic Happy Inspirational Romantic Sad Bright", test_size = 0.2, model = KNeighborsClassifier(n_neighbors=2), showScore = True, scaler=StandardScaler()):
+        
+        categories = categories.split()
+        
+        X_train, X_test, y_train, y_test, scaler, encoder = ERAS.__dataPreProcessing(dataset, categories, scaler)
+        
+        strtfdKFold = StratifiedKFold(n_splits=10)
+        kfold = strtfdKFold.split(X_train, y_train)
+        scores = []
+        
+        for k, (train, test) in enumerate(kfold):
+            model.fit(X_train[train], y_train[train])
+            score = model.score(X_train[test], y_train[test])
+            scores.append(score)
+            print('Fold: %2d, Training/Test Split Distribution: %s, Accuracy: %.3f' % (k+1, np.bincount(y_train[train]), score))
+    
+        print('\n\nCross-Validation accuracy: %.3f +/- %.3f' %(np.mean(scores), np.std(scores)))
+
+        #model = model.fit(X_train, y_train)
+        
+        if(showScore):
+           ERAS.__modelScore(categories, model, X_test, y_test)
+        
+        
+        ERAS.__model = model
+        ERAS.__encoder = encoder
+        ERAS.__scaler = scaler
+        return self
+
+    def predict(self, track, offset = None, duration = None, sr = 22050, window_seconds = 15, hop_seconds = 10, transcribe = False):
+        
+        if(ERAS.__model == None):
+            print("Model is not trained")
+            return
+        
+        y, sr = librosa.load(track, offset = offset, duration = duration, mono = True, sr = sr)
+
+        features = af.features_extractor_windowed(track = y, sr = sr, mfccs_n = 40, window_seconds = window_seconds, hop_seconds = hop_seconds)
+
+        prediction = ERAS.__encoder.inverse_transform(ERAS.__model.predict(ERAS.__scaler.transform(features)))
+
+        prediction = pd.DataFrame(prediction)
+
+        df = pd.DataFrame(columns=['FrameTime start', 'FrameTime End', 'Emotions'])
+        import numpy as np
+        df['Emotions'] = prediction
+        StartA = []
+        EndA = []
+        start = 0
+        for i in range(0,df.shape[0]):
+            StartA.append(start)
+            EndA.append(start+window_seconds)
+            start += hop_seconds
+        df['FrameTime start'] = StartA;
+        df['FrameTime End'] = EndA
+
+        colors = {'Angry': 'red',
+                'Inspirational': 'lightblue',
+                'Sad': 'gray',
+                'Dark': 'black',
+                'Happy': 'yellow',
+                'Bright': 'orange',
+                'Calm': 'green',
+                'Romantic': 'pink',
+                'Dramatic': 'purple'}
+        
+        fig, ax = plt.subplots(ncols=2, figsize=(18,8))
+        prediction = df['Emotions'].value_counts().sort_values(ascending=False);
+        img = df['Emotions'].value_counts().plot.pie(labels = prediction.index, colors=[colors[key] for key in prediction.index], ax=ax[0])
+
+        ax[0].set_title('Emotions pie plot')
+        #fig.colorbar(img, ax=ax[0])
+        ax[1].set_xlabel("Time")
+        ax[1].set_ylabel("Emotion")
+        ax[1].plot(df['FrameTime start'],df['Emotions'])
+        
+        plt.show()
+
+        fig = plt.figure(figsize=(20,6))
+        ax = fig.add_subplot(111)
+        ax.axes.get_yaxis().set_visible(False)
+        ax.set_aspect(1)
+        
+        times = np.arange(0, max(df['FrameTime End'][:]), hop_seconds)
+    
+        labels = [str(timedelta(seconds=int(seconds)))[2:] for seconds in times]
+        plt.xticks(times, labels)
+        plt.xlabel('Time')
+        plt.axis()
+        for row in df.iterrows():
+            x = row[1]['FrameTime start']
+            x1 = x + hop_seconds
+            plt.fill_between([x,x1], 1,y2=15, color=colors[row[1]['Emotions']])
+            plt.text((x+x1)/2,1,row[1]['Emotions'], horizontalalignment='center', verticalalignment='bottom')
+
+        plt.ylim(max(3,max(df['FrameTime End'][:])*3/100),1)
+        plt.show()
+
+        if(transcribe):
+            from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+            import whisper
+
+            whisper_model = whisper.load_model("small")
+            whisper_model.cuda(0)
+            result = whisper_model.transcribe(y)
+            print("Language: " + result['language'])
+            print(result["text"])
+            sid = SentimentIntensityAnalyzer()
+            print(sid.polarity_scores(result["text"]))
+
+        full = Audio(y,rate = sr)
+
+        display(full)
+        
+    def voiceEmotion(sr = 44100, seconds = 30):
+
+        chunk = 1024  # Record in chunks of 1024 samples
+        sample_format = pyaudio.paInt16  # 16 bits per sample
+        channels = 2
+        
+        filename = "Validation/output.wav"
+
+        p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+        print('Recording')
+
+        stream = p.open(format=sample_format,
+                        channels=channels,
+                        rate=fs,
+                        frames_per_buffer=chunk,
+                        input=True)
+
+        frames = []  # Initialize array to store frames
+
+        # Store data in chunks for 3 seconds
+        for i in range(0, int(sr / chunk * seconds)):
+            data = stream.read(chunk)
+            frames.append(data)
+
+        # Stop and close the stream 
+        stream.stop_stream()
+        stream.close()
+        # Terminate the PortAudio interface
+        p.terminate()
+
+        print('Finished recording')
+
+        # Save the recorded data as a WAV file
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(sample_format))
+        wf.setframerate(sr)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+        
+        ERAS.predict(track=filename)
---
+++ Emotion Recognition of Audio Signals/include/audioFeatures.py
@@ -0,0 +1,204 @@
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import pandas as pd
+import librosa
+import os
+import csv
+import warnings
+import scipy
+warnings.filterwarnings("ignore")
+
+class Extractor(object):
+    def __init__(self, emotions = "arrabbiato calmo cupo drammatico felice motivante romantico triste vivace"):
+        #super(Extractor, self).__init__(*args)
+        Extractor.__emotions = emotions.split()
+        
+    def __header_maker(self, mfccs_n = 40):
+        header = ""
+
+        for i in range(0, 12):
+            header += "chroma_mean_" + str(i) + " "
+        for i in range(0, 12):
+            header += "chroma_std_" + str(i) + " "
+
+        for i in range(0, mfccs_n):
+            header += "mfccs_mean_" + str(i) + " "
+        for i in range(0, mfccs_n):
+            header += "mfccs_std_" + str(i) + " "
+
+        header += "cent_mean cent_std cent_skew "
+
+        for i in range(0, 7):
+            header += "contrast_mean_" + str(i) + " "
+        for i in range(0, 7):
+            header += "contrast_std_" + str(i) + " "
+
+        header += "rolloff_mean rolloff_std rolloff_skew zrate_mean zrate_std zrate_skew tempo emotion"
+
+        return header.split()
+
+    def features_extractor(song_name = "", y = [], sr = 22500, mfccs_n = 40):
+        
+        if(song_name != ""): y, sr = librosa.load(song_name)
+        
+        y_harmonic, y_percussive = librosa.effects.hpss(y)
+
+        tempo, beat_frames = librosa.beat.beat_track(y=y_harmonic, sr=sr)
+        tempo = int(tempo)
+
+        chroma = librosa.feature.chroma_cens(y=y_harmonic, sr=sr)
+
+        cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+
+        contrast = librosa.feature.spectral_contrast(y=y_harmonic, sr=sr)
+
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+
+        zcr = librosa.feature.zero_crossing_rate(y_harmonic)
+
+        mfccs = librosa.feature.mfcc(y=y_harmonic, sr=sr, n_mfcc=mfccs_n)
+
+        chroma_mean = np.mean(chroma, axis=1)
+        chroma_std = np.std(chroma, axis=1)
+
+        chroma_df = pd.DataFrame()
+        for i in range(0, 12):
+            chroma_df["chroma_mean_" + str(i)] = chroma_mean[i]
+        for i in range(0, 12):
+            chroma_df["chroma_std_" + str(i)] = chroma_mean[i]
+        
+        chroma_df.loc[0] = np.concatenate((chroma_mean, chroma_std), axis=0)
+
+        mfccs_mean = np.mean(mfccs, axis=1)
+        mfccs_std = np.std(mfccs, axis=1)
+
+        mfccs_df = pd.DataFrame()
+        for i in range(0, mfccs_n):
+            mfccs_df["mfccs_mean_" + str(i)] = mfccs_mean[i]
+        for i in range(0, mfccs_n):
+            mfccs_df["mfccs_std_" + str(i)] = mfccs_mean[i]
+        mfccs_df.loc[0] = np.concatenate((mfccs_mean, mfccs_std), axis=0)
+
+        cent_mean = np.mean(cent)
+        cent_std = np.std(cent)
+        cent_skew = scipy.stats.skew(cent, axis=1)[0]
+
+        contrast_mean = np.mean(contrast, axis=1)
+        contrast_std = np.std(contrast, axis=1)
+
+        rolloff_mean = np.mean(rolloff)
+        rolloff_std = np.std(rolloff)
+        rolloff_skew = scipy.stats.skew(rolloff, axis=1)[0]
+
+        spectral_df = pd.DataFrame()
+        collist = ["cent_mean", "cent_std", "cent_skew"]
+        for i in range(0, 7):
+            collist.append("contrast_mean_" + str(i))
+        for i in range(0, 7):
+            collist.append("contrast_std_" + str(i))
+        collist = collist + ["rolloff_mean", "rolloff_std", "rolloff_skew"]
+        for c in collist:
+            spectral_df[c] = 0
+        data = np.concatenate(
+                (
+                    [cent_mean, cent_std, cent_skew],
+                    contrast_mean,
+                    contrast_std,
+                    [rolloff_mean, rolloff_std, rolloff_skew],
+                ),
+                axis=0,
+            )
+        spectral_df.loc[0] = data
+
+        zcr_mean = np.mean(zcr)
+        zcr_std = np.std(zcr)
+        zcr_skew = scipy.stats.skew(zcr, axis=1)[0]
+
+        zrate_df = pd.DataFrame()
+        zrate_df["zrate_mean"] = 0
+        zrate_df["zrate_std"] = 0
+        zrate_df["zrate_skew"] = 0
+        zrate_df.loc[0] = [zcr_mean, zcr_std, zcr_skew]
+
+        beat_df = pd.DataFrame()
+        beat_df["tempo"] = tempo
+        beat_df.loc[0] = tempo
+
+        to_append = pd.concat(
+                (chroma_df, mfccs_df, spectral_df, zrate_df, beat_df), axis=1
+            ).to_string(header=False, index=False, index_names=False)
+
+        return to_append.split()
+
+    def __csv_maker(file_name, data, permesso = "a"):
+        file = open(file_name, permesso, newline="")
+        with file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+            file.close()
+
+    def features_extractor_windowed(track, sr, window_seconds = 15, hop_seconds = 10, mfccs_n = 40):
+        duration = int(librosa.get_duration(y=track, sr=sr))
+        frame_len, hop_len = min(window_seconds, duration)*sr, min(hop_seconds, duration)*sr 
+        frames = librosa.util.frame(
+            track, frame_length=frame_len, hop_length=hop_len, axis=0)
+        data = pd.DataFrame()
+        for y in frames:
+            
+            data =  data.append(pd.Series(Extractor.features_extractor(y = y, sr = sr, mfccs_n = mfccs_n)), ignore_index = True)
+        
+        return np.array(data, dtype = float)
+
+    def dataset_maker(self,
+        path_tracks = "Tracks/",
+        file_name="dataset",
+        encoder=LabelEncoder(),
+        mfccs_n = 40,
+        sr = 22050,
+        window_seconds = 15,
+        hop_seconds = 10
+    ):
+        encoder = encoder.fit(Extractor.__emotions)
+        header = Extractor.__header_maker(mfccs_n)
+        
+        Extractor.__csv_maker(file_name + ".csv", header, "w")
+
+        Extractor.__csv_maker(file_name= file_name + "_metadata.csv", data="file_name start end classID class".split(), permesso="w")
+        
+        count = 0
+        for g in Extractor.__emotions:
+            for filename in os.listdir(f"{path_tracks}{g}"):
+                
+                songname = os.fspath(f"{path_tracks}{g}/{filename}")
+                
+                track, sr = librosa.load(songname, mono=True, sr = sr)
+
+                count += 1
+                print(str(count) + ")Nuova traccia: " + songname)
+                
+                frame_len, hop_len = window_seconds*sr, hop_seconds*sr  # 15s window con 10s hop
+                frames = librosa.util.frame(
+                    track, frame_length=frame_len, hop_length=hop_len, axis=0
+                )
+
+                start = 0
+                end = int(frame_len / sr)
+
+                for y in frames:
+                    
+                    data = Extractor.features_extractor(y = y, sr = sr, mfccs_n = mfccs_n)
+                    data.append(g)
+                    Extractor.__csv_maker(file_name + ".csv", data)
+                    
+                    data = [
+                        filename.encode("utf-8"),
+                        start,
+                        end,
+                        encoder.transform([g])[0],
+                        g,
+                    ]
+
+                    Extractor.__csv_maker(file_name + "_metadata.csv", data)
+                    
+                    start += int(hop_len / sr)
+                    end = start + int(frame_len / sr)
